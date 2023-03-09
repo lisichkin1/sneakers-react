90c3f4b90c8f9a56825fc523d602aa27d061f33a
Ivan <ivanlisichkin@icloud.com>
2023-03-09 22:56:48 +0400

    feat:add array sneakers

--- src/App.js
+++ src/App.js
@@ -1,90 +1,75 @@
 import Card from "./components/Card";
 import Header from "./components/Header";
+import Drawer from "./components/Drawer";
+
+const arrItem = [
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    imageURL: "/img/sneakers/image1.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 13999,
+    imageURL: "/img/sneakers/image2.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 11999,
+    imageURL: "/img/sneakers/image3.jpg",
+  },
+  {
+    name: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    imageURL: "/img/sneakers/image4.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Under Armour Curry 8",
+    price: 15499,
+    imageURL: "/img/sneakers/image5.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Kyrie 7",
+    price: 11299,
+    imageURL: "/img/sneakers/image6.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Jordan Air Jordan 11",
+    price: 10899,
+    imageURL: "/img/sneakers/image7.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike LeBron XVIII",
+    price: 16799,
+    imageURL: "/img/sneakers/image8.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Lebron XVIII Low",
+    price: 13199,
+    imageURL: "/img/sneakers/image9.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 10999,
+    imageURL: "/img/sneakers/image10.jpg",
+  },
+  {
+    name: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 7499,
+    imageURL: "/img/sneakers/image4.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Under Armour Curry 8",
+    price: 14399,
+    imageURL: "/img/sneakers/image5.jpg",
+  },
+];
 
 function App() {
   return (
     <div className="wrapper mx-auto mt-12 max-w-7xl box-border outline-none">
-      <div style={{ display:'none'}} className="drawer-container absolute top-0 left-0 z-10 w-full h-full">
-        <div className="drawer flex flex-col absolute w-[420px] h-full right-0 p-8 gap-y-8">
-          <div className="TitleCart">
-            <h3>Корзина</h3>
-            <button className="ButtonCardDelete">
-              <img
-                width={11}
-                height={11}
-                src="/img/plus.svg"
-                alt="plus button"
-              ></img>
-            </button>
-          </div>
-          <div className="CartList flex flex-col items-center justify-start gap-y-8 ">
-            <div className="CartItem">
-              <img
-                className="CartSneakerItem"
-                width={70}
-                height={70}
-                src="/img/sneakers/image5.jpg"
-                alt="sneakers"
-              ></img>
-              <div className="ItemDescription">
-                <p>Мужские Кроссовки Nike Air Max 270</p>
-                <b>12 999 руб.</b>
-              </div>
-              <div className="ButtonDelete">
-                <button className="ButtonCardDelete">
-                  <img
-                    width={11}
-                    height={11}
-                    src="/img/plus.svg"
-                    alt="plus button"
-                  ></img>
-                </button>
-              </div>
-            </div>
-            <div className="CartItem">
-              <img
-                className="CartSneakerItem"
-                width={70}
-                height={70}
-                src="/img/sneakers/image5.jpg"
-                alt="sneakers"
-              ></img>
-              <div className="ItemDescription">
-                <p>Мужские Кроссовки Nike Air Max 270</p>
-                <b>12 999 руб.</b>
-              </div>
-              <div className="ButtonDelete">
-                <button className="ButtonCardDelete">
-                  <img
-                    width={11}
-                    height={11}
-                    src="/img/plus.svg"
-                    alt="plus button"
-                  ></img>
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="PriceList">
-            <ul>
-              <li>
-                <span>Итого:</span>
-                <div></div>
-                <b>21 498 руб.</b>
-              </li>
-              <li>
-                <span>Налог 5%:</span>
-                <div></div>
-                <b>1074 руб.</b>
-              </li>
-            </ul>
-            <button className="GreenButton">
-              Оформить заказ <img src="/img/arrow.svg" alt="arrow"></img>
-            </button>
-          </div>
-        </div>
-      </div>
-      <Header/>
+      <Drawer />
+      <Header />
       <div className="content">
         <div className="flex items-center mb-10 justify-between">
           <h3>Все кроссовки</h3>
@@ -94,14 +79,13 @@ function App() {
           </div>
         </div>
         <div className="flex justify-start flex-wrap gap-x-[101px] gap-y-[40px]">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+          {arrItem.map((obj) => (
+            <Card 
+            title = {obj.name}
+            price = {obj.price}
+            imageURL = {obj.imageURL}
+            />
+          ))}
         </div>
       </div>
     </div>
