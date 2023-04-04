458e11f769d276b10eae28729c628104723c72de
Ivan <ivanlisichkin@icloud.com>
2023-04-04 18:00:29 +0400

    feat:add api

--- src/App.js
+++ src/App.js
@@ -1,76 +1,24 @@
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
-import { useState } from "react";
-
-const arrItem = [
-  {
-    "name": "Мужские Кроссовки Nike Blazer Mid Suede",
-    "price": 12999,
-    "imageURL": "/img/sneakers/image1.jpg"
-  },
-  {
-    "name": "Мужские Кроссовки Nike Air Max 270",
-    "price": 13999,
-    "imageURL": "/img/sneakers/image2.jpg"
-  },
-  {
-    "name": "Мужские Кроссовки Nike Blazer Mid Suede",
-    "price": 11999,
-    "imageURL": "/img/sneakers/image3.jpg"
-  },
-  {
-    "name": "Кроссовки Puma X Aka Boku Future Rider",
-    "price": 8999,
-    "imageURL": "/img/sneakers/image4.jpg"
-  },
-  {
-    "name": "Мужские Кроссовки Under Armour Curry 8",
-    "price": 15499,
-    "imageURL": "/img/sneakers/image5.jpg"
-  },
-  {
-    "name": "Мужские Кроссовки Nike Kyrie 7",
-    "price": 11299,
-    "imageURL": "/img/sneakers/image6.jpg"
-  },
-  {
-    "name": "Мужские Кроссовки Jordan Air Jordan 11",
-    "price": 10899,
-    "imageURL": "/img/sneakers/image7.jpg"
-  },
-  {
-    "name": "Мужские Кроссовки Nike LeBron XVIII",
-    "price": 16799,
-    "imageURL": "/img/sneakers/image8.jpg"
-  },
-  {
-    "name": "Мужские Кроссовки Nike Lebron XVIII Low",
-    "price": 13199,
-    "imageURL": "/img/sneakers/image9.jpg"
-  },
-  {
-    "name": "Мужские Кроссовки Nike Blazer Mid Suede",
-    "price": 10999,
-    "imageURL": "/img/sneakers/image10.jpg"
-  },
-  {
-    "name": "Кроссовки Puma X Aka Boku Future Rider",
-    "price": 7499,
-    "imageURL": "/img/sneakers/image4.jpg"
-  },
-  {
-    "name": "Мужские Кроссовки Under Armour Curry 8",
-    "price": 14399,
-    "imageURL": "/img/sneakers/image5.jpg"
-  },
-];
+import { useEffect, useState } from "react";
 
 function App() {
+  const [sneakersItems, setSneakersItems] = useState([])
+  const [cartSneakersItems, setCartSetSneakersItems] = useState([])
   const [cartOpen, setCartOpen] = useState(false)
+  useEffect(() => {
+    fetch('https://6423141677e7062b3e2a7b39.mockapi.io/items')
+    .then((res) =>{
+      return res.json();
+    })
+    .then((json) => {
+      setSneakersItems(json)
+    });
+  }, [])
   return (
     <div className="wrapper mx-auto mt-12 max-w-7xl box-border outline-none">
-      {cartOpen && <Drawer onCloseCart = {()=> setCartOpen(false)}/>}
+      {cartOpen && <Drawer sneakersItems = {cartSneakersItems} onCloseCart = {()=> setCartOpen(false)}/>}
       <Header onClickCart = {()=> setCartOpen(true)}/>
       <div className="content">
         <div className="flex items-center mb-10 justify-between">
@@ -81,7 +29,7 @@ function App() {
           </div>
         </div>
         <div className="flex justify-start flex-wrap gap-x-[101px] gap-y-[40px]">
-          {arrItem.map((obj) => (
+          {sneakersItems.map((obj) => (
             <Card 
             title = {obj.name}
             price = {obj.price}
