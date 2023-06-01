fd615a50b903abf0fea593eae8960862cb12dfe2
Ivan <ivanlisichkin@icloud.com>
2023-06-01 14:14:17 +0400

    feat:add routing and favorite

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import Card from "./components/Card";
+import { Route, Routes } from "react-router-dom"
+
+import Home from './Pages/Home';
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 import axios from "axios";
+import Favorites from "./Pages/Favorites";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -17,8 +20,10 @@ function App() {
     axios.get('https://6423141677e7062b3e2a7b39.mockapi.io/cart').then(res => {
       setCartSetSneakersItems(res.data)
     })
+    axios.get('https://646f20f609ff19b12086a2ff.mockapi.io/Favourites').then(res => {
+      setFavourites(res.data)
+    })
   }, [])
-
   const onAddToCart = (item) => {
     axios.post('https://6423141677e7062b3e2a7b39.mockapi.io/cart', item);
     setCartSetSneakersItems(prev => [...prev, item])
@@ -33,48 +38,43 @@ function App() {
     setSearchValue(event.target.value);
   }
 
-  const onAddToFavourites = (item) => {
-    axios.post('https://646f20f609ff19b12086a2ff.mockapi.io/Favourites', item);
-    setFavourites(prev => [...prev, item])
+  const onAddToFavourites = async (item) => {
+    try {
+      if(Favourites.find((favItem) => favItem.id === item.id)){
+        axios.delete(`https://646f20f609ff19b12086a2ff.mockapi.io/Favourites/${item.id}`)
+        setFavourites(prev => prev.filter(obj => obj.id !== item.id))
+      }
+      else{
+        const {data} = await axios.post('https://646f20f609ff19b12086a2ff.mockapi.io/Favourites', item);
+        setFavourites(prev => [...prev, data])
+      }
+    } catch (error) {
+      alert('Не удалось добавить в избранное')
+      
+    }
   }
-  
   return (
     <div className="wrapper mx-auto mt-12 max-w-7xl box-border outline-none">
       {cartOpen && <Drawer sneakersItems = {cartSneakersItems} onCloseCart = {()=> setCartOpen(false)} onDelete = {onDeleteItem}/>}
-      <Header onClickCart = {()=> setCartOpen(true)}/>
-      <div className="content">
-        <div className="flex items-center mb-10 justify-between">
-          <h3>{seacrValue ? `Поиск по: ${seacrValue}`: 'Все кроссовки'}</h3>
-          <div className="search-block">
-            <img src="/img/search.svg" alt="search"></img>
-            {
-            seacrValue && (<button className="ButtonCardDelete clear" onClick={() => setSearchValue('')}>
-            <img
-              width={11}
-              height={11}
-              src="/img/plus.svg"
-              alt="plus button"
-            ></img>
-            </button>)
-            }
-            <input onChange={onEditSearchInput} value={seacrValue} placeholder="Поиск... "></input>
-          </div>
-        </div>
-        <div className="flex justify-start flex-wrap gap-x-[101px] gap-y-[40px]">
-          {sneakersItems
-          .filter((obj)=>obj.name.toUpperCase().includes(seacrValue.toUpperCase()))
-          .map((obj) => (
-            <Card 
-            key = {obj.id}
-            title = {obj.name}
-            price = {obj.price}
-            imageURL = {obj.imageURL}
-            onClickPlusAdd={(item)=>onAddToCart(item)}
-            onClickLikeAdd = {(item)=>onAddToFavourites(item)}
-            />
-          ))}
-        </div>
-      </div>
+      
+        <Header onClickCart = {()=> setCartOpen(true)}/>
+        <Routes>
+          <Route 
+              path="/" element={  <Home
+              sneakersItems={sneakersItems}
+              seacrValue={seacrValue}
+              setSearchValue={setSearchValue}
+              onEditSearchInput={onEditSearchInput}
+              onAddToFavourites={onAddToFavourites}
+              onAddToCart={onAddToCart}
+            />}/>
+            <Route 
+              path="/favorites" element={  <Favorites 
+              sneakersItems={Favourites}
+              onAddToFavourites={onAddToFavourites}
+            />}/>
+        </Routes>
+    
     </div>
   );
 }
