7e8a90034a5545df331679a6e366803fe72ff34b
Ivan <ivanlisichkin@icloud.com>
2023-06-03 11:57:46 +0400

    fix:change margin

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -2,7 +2,8 @@ import Card from "../components/Card"
 
 function Favorites ({sneakersItems, onAddToFavourites}) {
     return(
-        
+      <div className="content">
+        <h3 className="mb-10">Мои закладки</h3>
         <div className="flex justify-start flex-wrap gap-x-[101px] gap-y-[40px]">
           {sneakersItems.map((obj) => (
             <Card 
@@ -12,6 +13,7 @@ function Favorites ({sneakersItems, onAddToFavourites}) {
             {...obj}
             />
           ))}
+        </div>
         </div>
       
     )
