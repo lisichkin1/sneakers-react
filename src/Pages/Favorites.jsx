4d608978d8aa75c084b4e3d3cfabdca46b7af261
Ivan <ivanlisichkin@icloud.com>
2023-06-05 18:10:41 +0400

    feat:add useContext for favorites

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -1,11 +1,14 @@
+import { useContext } from "react";
 import Card from "../components/Card";
+import AppContext from "../context";
 
-function Favorites({ sneakersItems, onAddToFavourites }) {
+function Favorites() {
+  const {Favourites, onAddToFavourites} = useContext(AppContext);
   return (
     <div className="content">
       <h3 className="mb-10">Мои закладки</h3>
       <div className="flex justify-start flex-wrap gap-x-[101px] gap-y-[40px]">
-        {sneakersItems.map((obj) => (
+        {Favourites.map((obj) => (
           <Card
             key={obj.id}
             favorited={true}
